785936f96475343aed4de84258611db8c59d0db7
pwhipp <paul.whipp@gmail.com>
2014-08-11 12:04:53 +1000

    Removed blanked try/except in setup.py

--- setup.py
+++ setup.py
@@ -12,37 +12,34 @@ install_requires = [
     "diff-match-patch",
 ]
 
-try:
-    setup(
-        name="mezzanine-wiki",
-        version=version,
-        author="Dmitry Falk",
-        author_email="dfalk5@gmail.com",
-        description="Wiki app for Mezzanine content management platform.",
-        long_description=open("README.rst").read(),
-        license="BSD",
-        #url="http://mezzanine.jupo.org/",
-        zip_safe=False,
-        include_package_data=True,
-        packages=find_packages(),
-        install_requires=install_requires,
-        entry_points="""
-            [console_scripts]
-        """,
-        classifiers=[
-            "Development Status :: 4 - Beta",
-            "Environment :: Web Environment",
-            "Framework :: Django",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: BSD License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python",
-            "Topic :: Internet :: WWW/HTTP",
-            "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-            "Topic :: Internet :: WWW/HTTP :: WSGI",
-            "Topic :: Software Development :: Libraries :: "
-                                                "Application Frameworks",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-        ],)
-except:
-    pass
+setup(
+    name="mezzanine-wiki",
+    version=version,
+    author="Dmitry Falk",
+    author_email="dfalk5@gmail.com",
+    description="Wiki app for Mezzanine content management platform.",
+    long_description=open("README.rst").read(),
+    license="BSD",
+    #url="http://mezzanine.jupo.org/",
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points="""
+        [console_scripts]
+    """,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "Topic :: Software Development :: Libraries :: "
+                                            "Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],)
